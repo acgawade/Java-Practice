e1cb30c0ed36544e54830cbcf65121cc0aac9ed3
MUMBAI1\AchyutG <achyut.gawade@citiustech.com>
2021-09-03 12:35:30 +0530

    Patterns package added with singletone pattern

---
+++ src/designPatterns/SingletoneDemo.java
@@ -0,0 +1,25 @@
+package designPatterns;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public final class SingletoneDemo {
+
+	private static SingletoneDemo singletoneDemoObj;
+	private static ReentrantLock reentrantLock = new ReentrantLock();
+
+	private SingletoneDemo() {
+
+	}
+
+	public static SingletoneDemo getSingletoneDemoInstance() {
+		if (singletoneDemoObj == null) {
+			reentrantLock.lock();
+			if (singletoneDemoObj == null) {
+				singletoneDemoObj = new SingletoneDemo();
+
+			}
+			reentrantLock.unlock();
+		}
+		return singletoneDemoObj;
+	}
+}
